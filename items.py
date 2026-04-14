d178f52fcc2975e089bc496e0ec9a7a76ac413bc
Morris <next16266@edu.nextkbh.dk>
2026-04-14 15:38:06 +0200

    Genopload

---
+++ items.py
@@ -0,0 +1,12 @@
+class apple():
+    # Skal kunne give en liv (feks 10 liv)
+
+
+class sword():
+    # Skal give dmg boost (feks 2x dmg)
+    # Har en varighed på 20 sek
+
+
+class boots():
+    # Skal give speed boost (feks 1.5x speed)
+    # Har en varighed på 30 sek
